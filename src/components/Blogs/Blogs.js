3cd38d99f3e2681078fb272a8e1b8f8c77df7bcc
mominulhoque250 <mominulhoque250@gmail.com>
2023-01-03 11:02:47 +0600

    blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1>Blogs website</h1>
+            <p> The Context API helps share data between components which you can't easily share with props, for example, complex data objects.</p>
+            <p>Semantic elements have meaningful names which tells about type of content. For example header, footer, table, … etc. HTML5 introduces <br></br>
+                many semantic elements as mentioned below which make the code easier to write and understand for the developer as well as instructs the browser on how to treat them. </p>
         </div>
     );
 };
